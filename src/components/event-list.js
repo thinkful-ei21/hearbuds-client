15989315434a3536b2096dacd4dc52445f9fc607
Sonya <sonjay08@gmail.com>
2018-08-22 16:55:05 -0500

    displays dummy data and redirects to single event page when button is clicked

--- src/components/event-list.js
+++ src/components/event-list.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
+import {getEventList} from '../actions/event-list'
+import {Redirect} from 'react-router-dom';
 
 export class EventList extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: null,
+        }
+    }
+    componentDidMount() {
+        this.setState({
+            redirect: null
+        })
+        this.props.dispatch(getEventList());
+    }
+
+    goToEvent(e) {
+        e.preventDefault();
+        console.log('clicked');
+        let eventId = e.currentTarget.value;
+        return this.setState({
+            redirect: <Redirect to={'/dashboard/'+eventId} />
+        })
+    }
 
     render() {
-        // eventually this will be populated with events
-        // from the API
-        // loop through the array of events and display a list
-        let eventList;
+        let eventList = this.props.eventList.map((event, index) => {
+            return (
+            <ul key={index.toString() + 'ul'}>
+                {this.state.redirect}
+                <li className='event-name' key={index.toString()+'name'}>{event.name}</li>
+                <li className='event-date' key={index.toString()+'date'}>{event.dates.start.localDate}</li>
+                <img className='event-img' src={event.images[0].url} alt='event artist' />
+                <li className='event-venue'key={index.toString()+'venue'}>{event.venues[0].name}</li>
+                <li className='event-rsvp-count' key={index.toString()+'rsvp'}>RSVPs:  {event.rsvpCount}</li>
+                <button type='submit' value={event.id} onClick={(e) => this.goToEvent(e)}>See more info</button>
+            </ul>
+            )
+        })
+
         return (
             <div>
                 {eventList}
+    
             </div>
         )
     }
@@ -20,7 +53,9 @@ export class EventList extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        // we'll put props here from redux state
+        eventList: state.event.eventList,
+        loading: state.event.loading,
+
     };
 }
 
