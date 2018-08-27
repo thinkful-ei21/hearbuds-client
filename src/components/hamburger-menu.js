60230a216879817b561ec070a8a01ea5c5ce0083
James Stuckey <jstuckey@rutgers.edu>
2018-08-27 17:26:51 -0400

    editing hamburger menu set up

--- src/components/hamburger-menu.js
+++ src/components/hamburger-menu.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class HamburgerMenu extends React.Component {
+export class HamburgerMenu extends React.Component {
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
+    }
+
     render(){
+        let logOutButton;
+        let editProfileLink;
+
+        // only render the buttons if logged in
+        if (this.props.loggedIn) {
+            logOutButton = (
+                <button onClick={() => this.logOut()}>Log out</button>
+            );
+            editProfileLink = (
+                <Link to="/edit">Edit Profile</Link>
+            )
+        }
 
         return (
             <div className="toggle">
@@ -9,7 +30,16 @@ export default class HamburgerMenu extends React.Component {
                 <div className="menu"></div>
                 <div className="menu"></div>
                 <div className="menu"></div>
+
+                {logOutButton}
+                {editProfileLink}
             </div>
         );
     }
 }
+
+const mapStateToprops = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToprops)(HamburgerMenu);
