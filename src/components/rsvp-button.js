4bfcd7a173c324442ff91515b1f37ff4601d6d14
James Stuckey <jstuckey@rutgers.edu>
2018-08-31 15:51:52 -0400

    makes the component only a functional one

--- src/components/rsvp-button.js
+++ src/components/rsvp-button.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { changeRsvp } from '../actions/single-event';
 
 export default function RSVPButton(props) {
 
     return <button onClick={() => props.onClick()}>RSVP to Event</button>;
 
 }
-
-// const mapStateToProps = state => {
-//     return {};
-// };
-
-// export default connect(mapStateToProps)(RSVPButton);
