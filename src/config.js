ad86a67cd1bd64c282f9a0f6a21e3ddb70304938
James Stuckey <jstuckey@rutgers.edu>
2018-09-06 13:44:15 -0400

    changed the server base url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://server-jbyioqpuzy.now.sh';
+    process.env.REACT_APP_API_BASE_URL || 'https://server-biywzhuynw.now.sh';
 export const EVENT_API_BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
