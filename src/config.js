833ed180ff666901b1ba0518953ce71a142d1cfe
Sonya <sonjay08@gmail.com>
2018-09-07 22:07:08 -0500

    increased margin on zipcode box for widescreens

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:4000' || 'https://server-zyeofwjpya.now.sh';
+export const API_BASE_URL = 'http://localhost:4000' || 'https://server-ltrhnybnqr.now.sh';
 export const EVENT_API_BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
